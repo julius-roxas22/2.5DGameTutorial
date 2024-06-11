23c90091f0f67d5177344bc8b5cc13764978c869
Julius Roxas <julius.roxas22@gmail.com>
2024-06-10 21:12:06 -0800

    basic character movement

---
+++ 2.5GameTutorial/Assets/IndieGameDeveloper/KeyboardInput/KeyboardInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieGameDeveloper
+{
+    public class KeyboardInput : MonoBehaviour
+    {
+        private void Update()
+        {
+            VirtualInputManager.Instance.MoveRight = Input.GetKey(KeyCode.D) ? true : false;
+            VirtualInputManager.Instance.MoveLeft = Input.GetKey(KeyCode.A) ? true : false;
+        }
+    }
+}
---
+++ 2.5GameTutorial/Assets/IndieGameDeveloper/SuitedMan/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieGameDeveloper
+{
+    public enum TransitionParameter
+    {
+        Move
+    }
+
+
+    public class PlayerControl : MonoBehaviour
+    {
+        public Animator animator;
+        public float movementSpeed;
+
+        void Update()
+        {
+            if (VirtualInputManager.Instance.MoveRight && VirtualInputManager.Instance.MoveLeft)
+            {
+                animator.SetBool(TransitionParameter.Move.ToString(), false);
+                return;
+            }
+
+            if (!VirtualInputManager.Instance.MoveRight && !VirtualInputManager.Instance.MoveLeft)
+            {
+                animator.SetBool(TransitionParameter.Move.ToString(), false);
+            }
+
+            if (VirtualInputManager.Instance.MoveRight)
+            {
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                animator.SetBool(TransitionParameter.Move.ToString(), true);
+            }
+
+            if (VirtualInputManager.Instance.MoveLeft)
+            {
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                animator.SetBool(TransitionParameter.Move.ToString(), true);
+            }
+        }
+    }
+}
+
